5d6e8365342e8abf8af4c32781a5a0a90e2b0ebc
Vitalii Pertsovych <oddisey000@gmail.com>
2024-02-11 10:46:31 +0200

    Starting notification project

---
+++ src/components/notification-button/notification.button.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import addNotification from 'react-push-notification';
+
+const NotificationButton = () => {
+/*const options = {
+  title: 'title',
+  subtitle: 'subtitle', //optional
+  message: 'message', //optional
+  //onClick: (e: Event | Notification) => void, //optional, onClick callback.
+  theme: 'red', //optional, default: undefined
+  duration: 3000, //optional, default: 5000,
+  backgroundTop: 'green', //optional, background color of top container.
+  backgroundBottom: 'darkgreen', //optional, background color of bottom container.
+  colorTop: 'green', //optional, font color of top container.
+  colorBottom: 'darkgreen', //optional, font color of bottom container.
+  closeButton: 'Go away', //optional, text or html/jsx element for close text. Default: Close,
+  //native?: boolean, //optional, makes the push notification a native OS notification
+  //icon?: string, // optional, Native only. Sets an icon for the notification.
+  //vibrate?: number | number[], // optional, Native only. Sets a vibration for the notification.
+  //silent?: boolean // optional, Native only. Makes the notification silent.
+};*/
+const buttonClick = () => {
+  addNotification({
+    title: 'Warning',
+    subtitle: 'This is a subtitle',
+    message: 'This is a very long message',
+    theme: 'red',
+    duration: 10000, //optional, default: 5000,
+    native: true, // when using native, your OS will handle theming.
+    autoCancel: false, // (optional) default: true
+    ongoing: false, // (optional) set whether this is an "ongoing" notification
+    ignoreInForeground: false, // (optional) if true, the notification will not be visible when the app is in the foreground (useful for parity with how iOS notifications appear). should be used in combine with `com.dieam.reactnativepushnotification.notification_foreground` setting
+  });
+};
+
+  return (
+    <div className="page">
+        <button onClick={buttonClick} className="button">
+          Hello world.
+        </button>
+    </div>
+  );
+}
+
+export default NotificationButton;
